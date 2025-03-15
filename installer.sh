53544dfbc4062ae26dc09ddbd26ab9908a5a04fe
Gerardo Miranda <gerardo.miranda.cespedes@gmail.com>
2025-03-14 22:26:37 -0400

    Fixed installer script not replacing existing script

--- installer.sh
+++ installer.sh
@@ -3,8 +3,8 @@
 sudo mkdir -p /opt/adep/
 
 cd /opt/adep/
-sudo wget https://raw.githubusercontent.com/gerardo-m/astro_deployer/refs/heads/master/adep.sh
-sudo wget https://raw.githubusercontent.com/gerardo-m/astro_deployer/refs/heads/master/adep-add.sh
+sudo wget -O adep.sh https://raw.githubusercontent.com/gerardo-m/astro_deployer/refs/heads/master/adep.sh
+sudo wget -O adep-add.sh https://raw.githubusercontent.com/gerardo-m/astro_deployer/refs/heads/master/adep-add.sh
 
 sudo chmod +x adep.sh
 sudo chmod +x adep-add.sh
